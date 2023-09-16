54b49dfd3fec9523d98d404a2bde3fcbe9fd366b
Dj <patelaxay177@gmail.com>
2023-09-16 14:20:19 +0530

    inch update

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function calculate() {
     document.getElementById('squareMeter').value = (first * second) + ' Sq foot';
     document.getElementById('squareFoot').value = ((first * second) * 10.7639).toFixed(2) + ' Sq foot';
     document.getElementById('var').value = ((first * second) * 10.7639 / 9).toFixed(2) + ' Var';
-    document.getElementById('inch').value = (first * 12) + ' Inch';
+    document.getElementById('inch').value = (first * 39.3701) + ' Inch';
     document.getElementById('subtract').value = (first - second).toFixed(2);
     document.getElementById('half').value = (first / 2).toFixed(2);
     document.getElementById('divide').value = (first / second).toFixed(2);
