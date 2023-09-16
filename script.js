e8f1d07937831c11b91615bee214723702b4ccd7
Dj <patelaxay177@gmail.com>
2023-09-16 15:16:48 +0530

    fixed after point 2 value

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ function calculate() {
     var first = parseFloat(document.getElementById('first').value);
     var second = parseFloat(document.getElementById('second').value);
 
-    document.getElementById('squareMeter').value = (first * second) + ' Sq meter';
+    document.getElementById('squareMeter').value = (first * second).toFixed(2) + ' Sq meter';
     document.getElementById('squareFoot').value = ((first * second) * 10.7639).toFixed(2) + ' Sq foot';
     document.getElementById('var').value = ((first * second) * 10.7639 / 9).toFixed(2) + ' Var';
-    document.getElementById('inch').value = (first * 39.3701) + ' Inch';
-    document.getElementById('inchToFoot').value = ((first * 39.3701) * 0.0833) + ' Inch to Foot';
+    document.getElementById('inch').value = (first * 39.3701).toFixed(2) + ' Inch';
+    document.getElementById('inchToFoot').value = ((first * 39.3701) * 0.0833).toFixed(2) + ' Inch to Foot';
     document.getElementById('subtract').value = (first - second).toFixed(2);
     document.getElementById('half').value = (first / 2).toFixed(2);
     document.getElementById('divide').value = (first / second).toFixed(2);
