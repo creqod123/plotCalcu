a3f491260954b647c4edecb5f4fe9aee35855dfd
Dj <patelaxay177@gmail.com>
2023-09-16 15:14:07 +0530

    inch to foor update

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function calculate() {
     document.getElementById('squareFoot').value = ((first * second) * 10.7639).toFixed(2) + ' Sq foot';
     document.getElementById('var').value = ((first * second) * 10.7639 / 9).toFixed(2) + ' Var';
     document.getElementById('inch').value = (first * 39.3701) + ' Inch';
-    document.getElementById('inchToFoot').value = ((first * 39.3701) * 12) + ' Inch to Foot';
+    document.getElementById('inchToFoot').value = ((first * 39.3701) * 0.0833) + ' Inch to Foot';
     document.getElementById('subtract').value = (first - second).toFixed(2);
     document.getElementById('half').value = (first / 2).toFixed(2);
     document.getElementById('divide').value = (first / second).toFixed(2);
